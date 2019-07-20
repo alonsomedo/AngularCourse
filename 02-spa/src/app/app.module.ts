a316877810243ebdc25c4a86aa4030bd5139f687
AlonsoMD <alonso2295@hotmail.com>
2019-07-20 10:36:13 -0500

    02_App_AddingRoutesInModule
    Importing routes in app.module

--- 02-spa/src/app/app.module.ts
+++ 02-spa/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+
+//Rutas
+import { FeatureRoutingModule } from './app.routes';
+
+
+//Servicios
 
 @NgModule({
   declarations: [
---
+++ 02-spa/src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { HomeComponent } from './components/home/home.component';
+import { Name2Component } from './';
+import { Name3Component } from './';
+import { Name4Component } from './';
+import { PageNotFoundComponent } from './';
+
+const ROUTES: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'path2', component: Name2Component },
+    { path: 'path3', component: Name3Component },
+    { path: 'path4', component: Name4Component },
+    { path: '**', component: PageNotFoundComponent },
+
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(ROUTES)],
+    exports: [RouterModule]
+})
+export class FeatureRoutingModule {}
