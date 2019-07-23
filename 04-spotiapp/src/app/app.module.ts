546d152873ed0ca9543f59fbcd9e89fb6d776aea
AlonsoMD <alonso2295@hotmail.com>
2019-07-22 22:48:24 -0500

    04_Import_HttpClientModule

--- 04-spotiapp/src/app/app.module.ts
+++ 04-spotiapp/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -25,6 +25,7 @@ import { ROUTES } from './app.routes';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot(ROUTES, {useHash:true})
   ],
   providers: [],
