c5efcfe1c4405b70ada66ec5995aca119aeba028
AlonsoMD <alonso2295@hotmail.com>
2019-07-22 21:41:07 -0500

    04_RoutesConfiguration

--- 04-spotiapp/src/app/app.routes.ts
+++ 04-spotiapp/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
-
-import { SearchComponent } from './components/search/search.component';
 import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
 
-const routes: Routes = [
+
+export const ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'search', component: SearchComponent },
     { path: '', pathMatch: 'full', redirectTo:'home' },
