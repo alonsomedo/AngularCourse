c8d6a77d37443aa664beccbe892ee81723b39274
AlonsoMD <alonso2295@hotmail.com>
2019-07-23 22:17:57 -0500

    04_TestingSpotifyService

---
+++ 04-spotiapp/src/app/services/spotify.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor(private http:HttpClient) {
+    console.log("Spotify service ready.")
+  }
+  
+  getNewReleases(){
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQBQHRvLC3IhBljf2GwAk6HWFh2Ul-HiXNYdPbiQwHmvE1witDCNSVwi-wBGc0GCCR4pmHpA30Ln8XhJOgg'
+    })
+
+    this.http.get('https://api.spotify.com/v1/browse/new-releases',{ headers })
+        .subscribe(data => {
+          console.log(data)
+        })
+    
+  }
+
+}
