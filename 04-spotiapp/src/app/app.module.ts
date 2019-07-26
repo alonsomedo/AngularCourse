b10d162e73344b29031c7677b06e4e299cf3994f
AlonsoMD <alonso2295@hotmail.com>
2019-07-25 23:03:04 -0500

    04_ImplementNoImagePipe

--- 04-spotiapp/src/app/app.module.ts
+++ 04-spotiapp/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { ROUTES } from './app.routes';
 
 //PIPES
 import { NoimagePipe } from './pipes/noimage.pipe';
+import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
 
 
 @NgModule({
@@ -26,7 +27,8 @@ import { NoimagePipe } from './pipes/noimage.pipe';
     SearchComponent,
     ArtistaComponent,
     NavbarComponent,
-    NoimagePipe
+    NoimagePipe,
+    TarjetasComponent
   ],
   imports: [
     BrowserModule,
