eeb35a7dc0e133c95230db357d90afedc414f513
AlonsoMD <alonso2295@hotmail.com>
2019-07-27 18:15:21 -0500

    05_CreatingList_ListItem

--- 05-deseos/src/app/services/deseos.service.ts
+++ 05-deseos/src/app/services/deseos.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Lista } from '../models/lista.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeseosService {
 
-  constructor() { }
+  lista: Lista[] = []
+
+  constructor() {
+    const LISTA1 = new Lista("Recolectar piedras del infinito.");
+    const LISTA2 = new Lista("Heroes a desaparecer.");
+    this.lista.push(LISTA1, LISTA2);
+
+  }
+
+  getListas() {
+
+  }
 }
