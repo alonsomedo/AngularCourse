d399d16faa1c1ecb5f361323a5ec57eeef737a3e
AlonsoMD <alonso2295@hotmail.com>
2019-07-27 22:46:31 -0500

    05_DeleteList

--- 05-deseos/src/app/components/listas/listas.component.ts
+++ 05-deseos/src/app/components/listas/listas.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
 import { Router } from '@angular/router';
+import { Lista } from '../../models/lista.model';
+
 
 @Component({
   selector: 'app-listas',
@@ -11,6 +13,7 @@ export class ListasComponent implements OnInit {
 
   @Input() terminado = true;
 
+
   constructor(public deseosService: DeseosService,
     private router: Router) {
 
@@ -18,13 +21,17 @@ export class ListasComponent implements OnInit {
 
   ngOnInit() { }
 
-  listaSeleccionada(listaId: number) {
+  listaSeleccionada(lista: Lista) {
     if (this.terminado) {
-      this.router.navigateByUrl(`tabs/tab2/agregar/${listaId}`);
+      this.router.navigateByUrl(`tabs/tab2/agregar/${lista.id}`);
     } else {
-      this.router.navigateByUrl(`tabs/tab1/agregar/${listaId}`);
+      this.router.navigateByUrl(`tabs/tab1/agregar/${lista.id}`);
     }
 
+  }
+
+  borrarLista(lista: Lista){
+    this.deseosService.borrarLista(lista);
   }
 
 
--- 05-deseos/src/app/services/deseos.service.ts
+++ 05-deseos/src/app/services/deseos.service.ts
@@ -43,4 +43,11 @@ export class DeseosService {
     }
 
   }
+
+
+  borrarLista(lista:Lista){
+    this.listas = this.listas.filter(listaData => listaData.id != lista.id )
+   
+ }
+
 }
