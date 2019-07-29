97ee13dfc63712191fcdeab5a78b37b77b8de064
AlonsoMD <alonso2295@hotmail.com>
2019-07-29 15:33:56 -0500

    07_LoginApp

---
+++ 07-bonus-loginApp/src/app/pages/registro/registro.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from '../../models/usuario.model';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+  usuario:UsuarioModel;
+
+  constructor() { }
+
+  ngOnInit() { 
+    this.usuario = new UsuarioModel();
+
+    this.usuario.email = "alonso2295@hotmail.com";
+  }
+
+  onSubmit(form:NgForm){
+    
+    if(form.invalid){return}
+
+    console.log(form)
+    console.log(this.usuario)
+  }
+
+}
