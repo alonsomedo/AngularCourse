87ebe2ab31980ac14d2f707fcfb977e8fbcff25b
AlonsoMD <alonso2295@hotmail.com>
2019-08-04 16:40:02 -0500

    09_HeroModel_Implementation

--- 09-heroesApp/src/app/app.module.ts
+++ 09-heroesApp/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HeroesComponent } from './pages/heroes/heroes.component';
 import { AppRoutingModule } from './app-routing.module';
+
+
+
 
 @NgModule({
   declarations: [
@@ -14,6 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
