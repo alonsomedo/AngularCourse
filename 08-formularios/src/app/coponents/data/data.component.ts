e5eef145593445b5965302bc855d4b30e1ca87fc
AlonsoMD <alonso2295@hotmail.com>
2019-08-04 11:51:04 -0500

    08_Data_LinkedViewAndValidators

--- 08-formularios/src/app/coponents/data/data.component.ts
+++ 08-formularios/src/app/coponents/data/data.component.ts
@@ -13,9 +13,12 @@ export class DataComponent implements OnInit {
   constructor() {
 
     this.forma = new FormGroup({
-      'nombre': new FormControl('Alonso'),
-      'apellido': new FormControl(),
-      'correo': new FormControl(),
+      'nombre': new FormControl('',Validators.required),
+      'apellido': new FormControl('', Validators.required),
+      'correo': new FormControl('', [
+                                      Validators.required, 
+                                      Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
+                                    ]),
     })
    }
 
