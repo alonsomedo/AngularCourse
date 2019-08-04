569ee986412357998ce6fed8acbbfeb93114cfd3
AlonsoMD <alonso2295@hotmail.com>
2019-08-04 12:39:13 -0500

    08_Validators_FormGroupName

--- 08-formularios/src/app/coponents/data/data.component.ts
+++ 08-formularios/src/app/coponents/data/data.component.ts
@@ -10,16 +10,30 @@ import { FormGroup,FormControl,Validators } from '@angular/forms';
 export class DataComponent implements OnInit {
 
   forma:FormGroup;
+
+  usuario:Object ={
+    nombreCompleto:{
+      nombre:'Alonso',
+      apellido:'Medina'
+    },
+    correo:'alonsomedo@ghotmail.com'
+  }
+
   constructor() {
+    
+    console.log(this.usuario)
 
     this.forma = new FormGroup({
-      'nombre': new FormControl('', [
-                                      Validators.required,
-                                      Validators.minLength(3),
-                                    ]),
-      'apellido': new FormControl('', [
-                                        Validators.required
-                                      ]),
+      
+      'nombreCompleto':new FormGroup({
+        'nombre': new FormControl('', [
+          Validators.required,
+          Validators.minLength(3),
+        ]),
+        'apellido': new FormControl('', [
+            Validators.required
+          ]),
+      }),
       'correo': new FormControl('', [
                                       Validators.required, 
                                       Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
