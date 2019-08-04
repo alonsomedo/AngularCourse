b62322a6c78c0fa5098a33b17320e8fab44a37d9
AlonsoMD <alonso2295@hotmail.com>
2019-08-04 17:32:03 -0500

    09_POST_and_PUT_Methods

--- 09-heroesApp/src/app/app.module.ts
+++ 09-heroesApp/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
@@ -19,6 +21,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
--- 09-heroesApp/src/app/pages/heroe/heroe.component.ts
+++ 09-heroesApp/src/app/pages/heroe/heroe.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroeModel } from '../../models/heroe.model';
 import { NgForm } from '@angular/forms';
+import { HeroesService } from '../../services/heroes.service';
+
 
 @Component({
   selector: 'app-heroe',
@@ -11,19 +13,30 @@ export class HeroeComponent implements OnInit {
 
   heroe:HeroeModel = new HeroeModel();
 
-  constructor() { }
+  constructor( private heroesService:HeroesService) { }
 
   ngOnInit() {
   }
 
   guardar(form:NgForm){
 
-    if(!form.invalid){
+    console.log(form)
+    if(form.invalid){
       console.log("Formulario invalido.")
       return;
     }
-    console.log(form)
-    console.log(this.heroe)
+
+    if(this.heroe.id){
+      this.heroesService.crearHeroe(this.heroe)
+      .subscribe( resp => console.log(resp))
+    }else{
+      this.heroesService.actualizarHeroe(this.heroe)
+      .subscribe( resp => console.log(resp))
+    }
+
+ 
+              
+
   }
 
 }
